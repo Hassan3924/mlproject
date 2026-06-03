d03b526b08c12b9f30ec00f3c5fa74825005ce93
Hassan3924 <hassanwork3924@gmail.com>
2026-06-03 22:43:44 +0200

    Started with Data Transofmration

---
+++ src/components/data_transformation.py
@@ -0,0 +1,13 @@
+import sys
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+class DataTransformation:
+    def __init__(self):
+        pass
+
+    def initiate_data_transformation(self):
+
+
+a
